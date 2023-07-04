be8da802446acd04ccafe0ae3ead1da327ddb606
Evgeniy <blbka@bk.ru>
2023-07-05 00:28:09 +0300

    задача 1

---
+++ Домашнее Задание 1/Program.cs
@@ -0,0 +1,7 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт большее число.
+Console.Write("input a first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("input a second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if(num1 > num2) Console.WriteLine(num1);
+else Console.WriteLine(num2);
