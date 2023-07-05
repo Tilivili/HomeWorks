43b3b0c71d8df7927c726302629532997195e76b
Evgeniy <blbka@bk.ru>
2023-07-05 16:42:52 +0300

    задача 3

--- Домашнее Задание 1/Program.cs
+++ Домашнее Задание 1/Program.cs
@@ -9,7 +9,7 @@ else Console.WriteLine(num2);
 */
 
 //Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
-
+/*
 Console.Write("input a first number: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("input a second number: ");
@@ -22,3 +22,20 @@ if (num2 > max) max = num2;
 if (num3 > max) max = num3;
 Console.Write("max = ");
 Console.WriteLine(max);
+*/
+
+//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+//является ли число чётным (делится ли оно на два без остатка).
+/*
+Console.Write("input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num % 2 == 0)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("no");
+}
+*/
+
