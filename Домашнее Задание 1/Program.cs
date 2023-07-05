15acf5c1dd36a5c003fd69afabeb1ae944eddbdb
Evgeniy <blbka@bk.ru>
2023-07-05 16:28:43 +0300

    задача 2

--- Домашнее Задание 1/Program.cs
+++ Домашнее Задание 1/Program.cs
@@ -1,7 +1,24 @@
 ﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт большее число.
+/*
 Console.Write("input a first number: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("input a second number: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
 if(num1 > num2) Console.WriteLine(num1);
 else Console.WriteLine(num2);
+*/
+
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Write("input a first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("input a second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("input a third number: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+if (num1 > max) max = num1;
+if (num2 > max) max = num2;
+if (num3 > max) max = num3;
+Console.Write("max = ");
+Console.WriteLine(max);
