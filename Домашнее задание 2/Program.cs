72d6417811272b38661040089115a1acfcac9f7c
Evgeniy <blbka@bk.ru>
2023-07-08 11:13:29 +0300

    первая задача

---
+++ Домашнее задание 2/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+/*
+int CutNumber(int num)
+{
+    int num1 = num % 100;
+    int num2 = num1 / 10;
+    int result = num2;
+    return result;
+}
+Console.Write("input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int newNumber = CutNumber(num); 
+Console.WriteLine($"New version number of {num} is {newNumber}");
+*/
+
+//Задача 13: Напишите программу, которая выводит третью цифру заданного числа.
